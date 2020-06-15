3837e25c4606bed1b02ef8a1300521d0d7444d57
Mlipphardt <matthewlipphardt@gmail.com>
2020-06-15 12:47:32 -0400

    Animations now trigger when user first scrolls to element

--- assets/javascript/fadein.js
+++ assets/javascript/fadein.js
@@ -4,22 +4,17 @@ $(document).ready(function () {
    $("#banner").addClass("visible");
    $("#namewrapper").addClass("visible");
 });
-
-// When user scrolls, elements fade into view
-$(document).on("scroll", function () {
-    var pageTop = $(document).scrollTop()
-    var pageBottom = pageTop + $(window).height()
-    var tags = $("div") 
-
-    for (let i = 0; i < tags.length; i++) {
-        let tag = tags[i];
-        if ($(tag).position().top < pageBottom) { 
-            $(tag).addClass("visible")
-          }
-        else {
-        $(tag).removeClass("visible")
-        }        
-    }
-    
-})
 */
+// When user scrolls, if element has an animation, play it.
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $("div");
+
+  for (let i = 0; i < tags.length; i++) {
+    let tag = tags[i];
+    if ($(tag).position().top < pageBottom && $(tag).attr("data-animation")) {
+      $(tag).addClass($(tag).attr("data-animation"));
+    }
+  }
+});
