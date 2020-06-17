ad03d49288aef8f38937bf5935240c51009fbeb9
Mlipphardt <matthewlipphardt@gmail.com>
2020-06-17 11:41:07 -0400

    Removed some test logging, changed spacing, chart only draws once now

--- assets/javascript/fadein.js
+++ assets/javascript/fadein.js
@@ -20,21 +20,70 @@ $(document).on("scroll", function () {
   }
 });
 */
+let chartDrawn = false;
 
+//After document has loaded, detect scrolling and see if animated elements need to fire.
 $(document).ready(function () {
   $(document).on("scroll", function () {
     var pageTop = $(document).scrollTop();
     var pageBottom = pageTop + $(window).height();
     var tags = $("div");
 
+    if (
+      $("#skills-donut").offset().top + $("#skills-donut").outerHeight() <
+        pageBottom &&
+      chartDrawn === false
+    ) {
+      chartDrawn = true;
+      var ctx = document.getElementById("skills-donut").getContext("2d");
+      var myChart = new Chart(ctx, {
+        type: "doughnut",
+        data: {
+          labels: [
+            "JavaScript",
+            "CSS",
+            "HTML",
+            "PHP",
+            "React",
+            "Node",
+            "MySQL",
+            "MongoDB",
+          ],
+          datasets: [
+            {
+              data: [20, 20, 20, 10, 10, 10, 5, 5],
+              backgroundColor: [
+                "blue",
+                "red",
+                "green",
+                "yellow",
+                "orange",
+                "purple",
+                "pink",
+                "violet",
+              ],
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+        },
+      });
+    }
+
     for (let i = 0; i < tags.length; i++) {
       let tag = tags[i];
-      if ($(tag).position().top < pageBottom && $(tag).attr("data-animation")) {
+
+      if (
+        $(tag).offset().top + $(tag).outerHeight() < pageBottom &&
+        $(tag).attr("data-animation")
+      ) {
         $(tag).addClass($(tag).attr("data-animation"));
       }
     }
   });
 
+  /*    
   var ctx = document.getElementById("skills-donut").getContext("2d");
   var myChart = new Chart(ctx, {
     type: "doughnut",
@@ -69,4 +118,5 @@ $(document).ready(function () {
       responsive: true,
     },
   });
+*/
 });
