cc0c55425773f2ed9706205720ff56b308523800
Mlipphardt <matthewlipphardt@gmail.com>
2020-06-20 01:13:42 -0400

    Created constructor for projects

--- assets/javascript/modal.js
+++ assets/javascript/modal.js
@@ -1,17 +1,65 @@
-let titles = [
-  "SpartanFit",
-  "Your Library",
-  "Haggle-Haus",
-  "Centralized News",
-  "Fanfixx",
-  "Deep Sea Memory Game",
+//Constructor for projects for filling modal content dynamically
+function Project(title, summary, tech, githubLink, liveLink) {
+  this.title = title;
+  this.githubLink = githubLink;
+  this.liveLink = liveLink;
+  this.summary = summary;
+  this.tech = tech;
+}
+
+let projects = [
+  new Project(
+    "SpartanFit",
+    "SpartanFit is a project",
+    "SpartanFit used tech",
+    "www.google.com",
+    "wwww.google.com"
+  ),
+  new Project(
+    "SpartanFit",
+    "SpartanFit is a project",
+    "SpartanFit used tech",
+    "www.google.com",
+    "wwww.google.com"
+  ),
+  new Project(
+    "SpartanFit",
+    "SpartanFit is a project",
+    "SpartanFit used tech",
+    "www.google.com",
+    "wwww.google.com"
+  ),
+  new Project(
+    "SpartanFit",
+    "SpartanFit is a project",
+    "SpartanFit used tech",
+    "www.google.com",
+    "wwww.google.com"
+  ),
+  new Project(
+    "SpartanFit",
+    "SpartanFit is a project",
+    "SpartanFit used tech",
+    "www.google.com",
+    "wwww.google.com"
+  ),
+  new Project(
+    "SpartanFit",
+    "SpartanFit is a project",
+    "SpartanFit used tech",
+    "www.google.com",
+    "wwww.google.com"
+  ),
 ];
 
 $(document).ready(function () {
+  //When learn-more-btn is clicked, toggle modal and fill with appropriate content
   $(".learn-more-btn").on("click", function (event) {
     event.preventDefault();
     $("#project-modal").modal("toggle");
-    let project = $(this).attr("data-project-id");
-    $(".modal-title").text(titles[project]);
+    let project = projects[$(this).attr("data-project-id")];
+    $(".modal-title").text(project.title);
+    $("#modal-summary").text(project.summary);
+    $("#modal-tech").text(project.tech);
   });
 });
